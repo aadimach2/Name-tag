e88b6098127e2f13be592b53562b1a20a853393a
aadimach2 <147931709+aadimach2@users.noreply.github.com>
2023-10-22 22:17:37 +0530

    Add files via upload

---
+++ Name tag/index.js
@@ -0,0 +1,17 @@
+const name=document.getElementById("name");
+
+
+const tagsContainer=document.querySelector(".tagss");
+
+const addName=()=>{
+    if(name.value==""){
+        return null;
+    }
+    const tag=document.createElement("div");
+    tag.setAttribute("class","tag");
+
+
+
+    tag.innerText=`Hey,I am ${name.value}`; 
+    tagsContainer.append(tag);
+};
